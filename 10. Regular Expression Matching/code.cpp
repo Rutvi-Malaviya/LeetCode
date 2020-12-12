c7ae8db37c297eac26abd7b82bc387337f3caa97
Rutvi Malaviya <60802831+Rutvi-Malaviya@users.noreply.github.com>
2020-12-12 13:34:42 +0530

    Create code.cpp

---
+++ 10. Regular Expression Matching/code.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if(p.size()==0) return (s.size()==0);
+        
+        bool ans = (s.size()>0 && (s[0]==p[0] || p[0]=='.'));
+        
+        if(p.size()>1 && p[1]=='*'){
+            return (ans && isMatch(s.substr(1,s.size()-1), p)) ||
+                    isMatch(s, p.substr(2,p.size()-2));
+        }
+        else{
+            return ans && isMatch(s.substr(1,s.size()-1), p.substr(1,p.size()-1));
+        }
+    }
+};
